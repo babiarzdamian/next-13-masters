ec5b1832695cc53cd38ea39bc2c7eafc94b8f5ed
Damian Babiarz <babiarz_damian@wp.pl>
2023-09-06 21:41:31 +0200

    style: update global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Rubik } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -12,7 +12,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={`${rubik.className} min-h-screen py-12`}>
+				{children}
+			</body>
 		</html>
 	);
 }
