1740218953337f1ddb8286aaf8c12f4b3d1fb9e0
Damian Babiarz <babiarz_damian@wp.pl>
2023-09-11 21:20:30 +0200

    feat: implement active navbar link component, update next routing options (2.6)

--- src/ui/atoms/NavbarMenuItem.tsx
+++ src/ui/atoms/NavbarMenuItem.tsx
@@ -1,18 +1,30 @@
+"use client";
+
 import Link from "next/link";
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
+import { type Route } from "next";
 
 type NavItemProps = {
-	href: string;
+	href: Route;
 	title: string;
 	icon?: React.ReactNode;
-	classList?: string;
+	className?: string;
 };
 
-export const NavbarMenuItem = ({ href, title, icon, classList }: NavItemProps) => {
+export const NavbarMenuItem = ({ href, title, icon, className }: NavItemProps) => {
+	const pathname = usePathname();
+	const isActive = pathname.split("/")[1] === href.split("/")[1];
+
 	return (
 		<li>
 			<Link
 				href={href}
-				className={`rounded-lg px-4 py-2 font-bold uppercase text-gray-700 ${classList}`}
+				className={clsx(
+					"border-b-4 px-4 py-4 font-bold uppercase text-gray-700 hover:border-b-orange-300",
+					isActive ? `border-b-orange-300` : `border-b-transparent`,
+					className,
+				)}
 				aria-label={icon ? title : undefined}
 			>
 				{icon || title}
