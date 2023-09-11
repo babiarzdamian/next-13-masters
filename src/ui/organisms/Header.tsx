d6c23dbf47c3e34a4ec5723b48be21e2c4fd024d
Damian Babiarz <babiarz_damian@wp.pl>
2023-09-11 21:27:22 +0200

    fix: update header z-index

--- src/ui/organisms/Header.tsx
+++ src/ui/organisms/Header.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "@molecules/Navbar";
 
 export const Header = () => {
 	return (
-		<header className="sticky top-0 mx-auto">
+		<header className="sticky top-0 z-10 mx-auto">
 			<Navbar />
 		</header>
 	);
